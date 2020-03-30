461c655415ac8a4aef987e6a181777939ce07c55
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-03-30 15:53:35 +0200

    NOW WITH LESS XSS

--- call.js
+++ call.js
@@ -6,8 +6,8 @@ console.log(code)
 const name = urlParams.get('name')
 console.log(name)
 
-document.getElementById('name').innerHTML = name
-document.getElementById('code').innerHTML = code
+document.getElementById('name').innerText = name
+document.getElementById('code').innerText = code
 
 console.log("Hello");
 setTimeout(() => { prank(); }, 4000);
